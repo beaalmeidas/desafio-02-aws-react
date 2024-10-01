f804678decb5a5dad78ef79756272f318a445245
beaalmeidas <beatrizalmeidabss@gmail.com>
2024-10-01 15:43:33 -0300

    feat: componente de produto comic feito

---
+++ uol-comics/src/components/comic-card/comicCard.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './comicCard.css';
+
+interface ComicCardProps {
+    cover_image: string;
+    title: string;
+    price: number;
+    author: string;
+    release_year: number;
+}
+
+const ComicCard: React.FC<ComicCardProps> = ({ cover_image, title, price, author, release_year }) => {
+    return (
+        <div className="comic-card">
+          <img src={cover_image} alt={title} className="comic-cover-image" />
+          
+          <div className="comic-info">
+            <h2 className="comic-title">{title}</h2>
+            <p className="comic-price">$ {price.toFixed(2)}</p>
+            <p className="comic-author">{author}</p>
+            <p className="comic-year">{release_year}</p>
+          </div>
+        </div>
+    );
+};
+
+export default ComicCard;
