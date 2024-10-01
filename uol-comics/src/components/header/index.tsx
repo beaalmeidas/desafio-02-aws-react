3d9cf004c01fe1659bd60a758e0af24ffea8bd40
Rafael Soares <rafael.soares.pb@compasso.com.br>
2024-10-01 20:46:36 -0300

    feat: Finalizado a estruturação do Header

--- uol-comics/src/components/header/index.tsx
+++ uol-comics/src/components/header/index.tsx
@@ -1,8 +1,25 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './style.css'
 
-const Header = () => {
+type Props = {
+    showFilter: boolean
+}
+
+const Header = (props: Props) => {
+    const {showFilter} = props
+
+    // Modify States
+    const [filterState, setFilterState] = useState(true)
+    const [hasCartItem, setHasCartItem] = useState(false)
+
+    useEffect(()=>{
+        setFilterState(showFilter)
+    },[props])
+
+    // Defaul functions
     const [inputLabelVisible,setInputLabelVisible] = useState(true);
+    const [cartImage,setCartImage] = useState('./assets/svg/cart.svg');
+    const [logoutImage,setLogoutImage] = useState('./assets/images/logout.png');
 
     const filterInputRef = useRef<HTMLInputElement>(null);
 
@@ -20,18 +37,22 @@ const Header = () => {
             </section>
 
             <section className="name-filter">
-                {inputLabelVisible && (
-                    <label onClick={setFocus}>
-                        <img src="./assets/images/symbols-search.png" alt="pesquisa"/>
-                        <p className="font-regular">Pesquisar por nome...</p>
-                    </label>
-                )}
+            {filterState && (
+                <>
+                    {inputLabelVisible && (
+                        <label onClick={setFocus}>
+                            <img src="./assets/images/symbols-search.png" alt="pesquisa"/>
+                            <p className="font-regular">Pesquisar por nome...</p>
+                        </label>
+                    )}
 
-                <input name="filter"
-                onFocus={() => {setInputLabelVisible(false)}}
-                onBlur={() => {setInputLabelVisible(true)}}
-                ref={filterInputRef}
-                />
+                    <input name="filter"
+                    onFocus={() => {setInputLabelVisible(false)}}
+                    onBlur={() => {setInputLabelVisible(true)}}
+                    ref={filterInputRef}
+                    />
+                </>
+            )}
             </section>
 
             <section className="right-side-header">
@@ -41,8 +62,25 @@ const Header = () => {
                     <a href="#"><p className="font-regular">Personagens</p></a>
                 </div>
                 <a href="#">
-                    <img className="cart-icon" src="./assets/svg/cart.svg" alt="Carrinho" />
+                    <div className="go-to-cart">
+                        <img
+                        onMouseEnter={()=>{setCartImage('./assets/images/cart-hover.png')}}
+                        onMouseLeave={()=>{setCartImage('./assets/svg/cart.svg')}}
+                        className="cart-icon" src={cartImage} alt="Carrinho"/>
+
+                        {hasCartItem && (
+                            <div className="pointer"/>
+                        )}
+                    </div>
                 </a>
+
+                <button className="logout-button normal-button button-square"
+                onMouseEnter={()=>{setLogoutImage('./assets/images/logout-hover.png')}}
+                onMouseLeave={()=>{setLogoutImage('./assets/images/logout.png')}}
+                >
+                    <img src={logoutImage} alt="logout"/>
+                    <p>Sair</p>
+                </button>
             </section>
         </header>
     )
