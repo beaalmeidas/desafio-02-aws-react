69c43414e89301e2bb97e09780143f1b8a20a762
Debora Oliveira <oliveirasilvadebora@hotmail.com>
2024-10-03 11:41:44 -0300

    refactor: removendo linhas desnecessárias e arrumando identação

--- uol-comics/src/components/forms/login/formLogin.tsx
+++ uol-comics/src/components/forms/login/formLogin.tsx
@@ -9,13 +9,12 @@ const FormLogin = ({onSubmit}: LoginProps) => {
 return (
     <form onSubmit={handleSubmit(onSubmit)}>
     <div className="FormFiels">
-    <input type="email" id="email" placeholder="Email" {...register("email")}/>
-    {errors.email && <p className="error">{errors.email.message}</p>}
-    <input type="password" id="password" placeholder="Senha" {...register("password")}/>
-    {errors.password && <p className="error">{errors.password.message}</p>}
+        <input type="email" id="email" placeholder="Email" {...register("email")}/>
+        {errors.email && <p className="error">{errors.email.message}</p>}
+        <input type="password" id="password" placeholder="Senha" {...register("password")}/>
+        {errors.password && <p className="error">{errors.password.message}</p>}
     </div>
     <button type="submit" className="submit-button">Criar conta</button>
-    <p className="createAccountText">Já tem uma conta? <a href="#">Clique aqui!</a></p>
 </form>
 )
 }
--- uol-comics/src/components/forms/signup/formSignUp.tsx
+++ uol-comics/src/components/forms/signup/formSignUp.tsx
@@ -10,16 +10,15 @@ return (
     <form onSubmit={handleSubmit(onSubmit)}>
         <div className="FormFiels">
         <input type="text" id="name" placeholder="Nome completo" {...register("username")} />
-        {errors.username && <p className="error">{errors.username.message}</p>}
-        <input type="email"  id="email" placeholder="Email" {...register("email")}/>
-        {errors.email && <p className="error">{errors.email.message}</p>}
-        <input type="password" id="password" placeholder="Senha" {...register("password")}/>
-        {errors.password && <p className="error">{errors.password.message}</p>}
-        <input type="password"  id="confimPassword" placeholder="Confirme a senha" {...register("confirmPassword")}/>
-        {errors.confirmPassword && <p className="error">{errors.confirmPassword.message}</p>}
+            {errors.username && <p className="error">{errors.username.message}</p>}
+            <input type="email"  id="email" placeholder="Email" {...register("email")}/>
+            {errors.email && <p className="error">{errors.email.message}</p>}
+            <input type="password" id="password" placeholder="Senha" {...register("password")}/>
+            {errors.password && <p className="error">{errors.password.message}</p>}
+            <input type="password"  id="confimPassword" placeholder="Confirme a senha" {...register("confirmPassword")}/>
+            {errors.confirmPassword && <p className="error">{errors.confirmPassword.message}</p>}
         </div>
         <button type="submit" className="submit-button">Criar conta</button>
-        <p className="createAccountText">Já tem uma conta? <a href="#">Clique aqui!</a></p>
     </form>
 )
 }
