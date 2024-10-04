25303b8a5328b091b626a7224615954ec10e3491
Debora Oliveira <oliveirasilvadebora@hotmail.com>
2024-10-04 20:32:42 -0300

    feat: adicionando classes no FormLogin

--- uol-comics/src/components/forms/login/formLogin.tsx
+++ uol-comics/src/components/forms/login/formLogin.tsx
@@ -1,5 +1,6 @@
 import { useLogin} from "../../../hooks/useLogin"
 import { LoginSchema } from "../../../schemas/LoginSchema"
+import "./styles.css"
 interface LoginProps {
     onSubmit: (data: LoginSchema) => void
 }
@@ -9,12 +10,12 @@ const FormLogin = ({onSubmit}: LoginProps) => {
 return (
     <form onSubmit={handleSubmit(onSubmit)}>
     <div className="FormFiels">
-        <input type="email" id="email" placeholder="Email" {...register("email")}/>
+        <input type="email" id="email" placeholder="Email" className="e-mail-input" {...register("email")}/>
         {errors.email && <p className="error">{errors.email.message}</p>}
-        <input type="password" id="password" placeholder="Senha" {...register("password")}/>
+        <input type="password" id="password" placeholder="Senha" className="password-input" {...register("password")}/>
         {errors.password && <p className="error">{errors.password.message}</p>}
     </div>
-    <button type="submit" className="submit-button">Criar conta</button>
+    <button type="submit" className="card-button">Entrar</button>
 </form>
 )
 }
