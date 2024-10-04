71be2965a6ba8ce5cb0bb19759951c3c886613ea
beaalmeidas <beatrizalmeidabss@gmail.com>
2024-10-04 15:49:03 -0300

    fix: resolvido erro na resposta da api

--- uol-comics/src/routes/appRoutes.tsx
+++ uol-comics/src/routes/appRoutes.tsx
@@ -3,14 +3,15 @@ import { DefaultLayout } from "../layouts/DefaultLayout";
 
 import ComicsPage from "../pages/ComicsPage";
 import { CharacterPage } from "../pages/CharacterPage";
-import { HomePage } from "../pages/homePage";
+import { HomePage } from "../pages/HomePage";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<HomePage />} />
-        <Route path="/comics-page" element={<ComicsPage />} />
+        <Route path="/comics-list" element={<ComicsPage />} />
+        <Route path="/comic-details/" element={<ComicsPage />} />
         <Route path="/character-page" element={<CharacterPage />} />
       </Route>
     </Routes>
