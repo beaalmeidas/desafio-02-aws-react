d92ff2641b229a49ad2964ab09e76cf1ca6b7b18
Debora Oliveira <oliveirasilvadebora@hotmail.com>
2024-10-05 11:41:38 -0300

    feat: criando as respostas visuais de login no UserForm.tsx

--- uol-comics/src/components/forms/user/UserForm.tsx
+++ uol-comics/src/components/forms/user/UserForm.tsx
@@ -8,14 +8,29 @@ import "./styles.css"
 
 const UserForm = () => {
     const [isUserLogged, setIsUserLogged] = useState(true)
+    const [loadingPage, setLoadingPage] = useState(false)
     const pageNavegation = useNavigate()
-    const handleUserLogin = (data: LoginSchema) => {
-        localStorage.setItem("UserLogInInfo", JSON.stringify(data))
-        pageNavegation("/")
+    const handleUserLogin = async (data: LoginSchema) => {
+        setLoadingPage(true)
+        try{
+            await new Promise((res) => setTimeout(res,4000))
+            localStorage.setItem("UserLogInInfo", JSON.stringify(data))
+            alert("Login finalizado com sucesso!")
+            pageNavegation("/")
+        }finally{
+            setLoadingPage(false)
     }
-    const handleUserSignIn = (data: SignUpSchema) => {
-        localStorage.setItem("userSignInInfo", JSON.stringify(data))
-        pageNavegation("/")
+}
+    const handleUserSignIn = async (data: SignUpSchema) => {
+        setLoadingPage(true)
+        try{
+            await new Promise((res) => setTimeout(res,4000))
+            localStorage.setItem("userSignInInfo", JSON.stringify(data))
+            alert("Cadastro finalizado com sucesso!")
+            pageNavegation("/")
+        }finally{
+            setLoadingPage(false)
+        }
     }
     return (
     <div className="main-container">
@@ -30,9 +45,10 @@ const UserForm = () => {
         <h1 className="card-title">{isUserLogged ? "Escolha seu herói" : "Crie seu herói"}</h1>
         {isUserLogged ? (<FormLogin onSubmit={handleUserLogin}/>) : 
             (<FormSignUp onSubmit={handleUserSignIn} />)}
-        <p className="card-text">{isUserLogged ? "Ainda não tem uma conta?" : "já tem uma conta?"}
+            {loadingPage && <p className="loading-text">{isUserLogged ? "Aguarde. Logando na sua conta...": "Aguarde. Criando conta..."}</p>}
+        <p className="card-text">{isUserLogged ? "Ainda não tem uma conta?" : "Já tem uma conta?"}
             <a className=" card-text card-link" href="#" onClick={() => setIsUserLogged(!isUserLogged)}>
-                {isUserLogged? "Clique aqui!": "Clique aqui!"}
+                {isUserLogged? " Clique aqui!": " Clique aqui!"}
             </a>
         </p>
         </div>
