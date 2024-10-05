fef1db88d9d6f0f8f427a47db842aa0e67df8c2a
Debora Oliveira <oliveirasilvadebora@hotmail.com>
2024-10-05 10:11:21 -0300

    feat: ajustes na estilização desktop  do FormSignUp.tsx

--- uol-comics/src/components/forms/signup/formSignUp.tsx
+++ uol-comics/src/components/forms/signup/formSignUp.tsx
@@ -9,16 +9,16 @@ const FormSignUp = ({onSubmit}: SignUpProps) => {
 return (
     <form onSubmit={handleSubmit(onSubmit)}>
         <div className="FormFiels">
-        <input type="text" id="name" placeholder="Nome completo" {...register("username")} />
+        <input type="text" id="name" placeholder="Nome completo" {...register("username")} className="name-input"/>
             {errors.username && <p className="error">{errors.username.message}</p>}
-            <input type="email"  id="email" placeholder="Email" {...register("email")}/>
+            <input type="email"  id="email" placeholder="Email" {...register("email")} className="e-mail-input"/>
             {errors.email && <p className="error">{errors.email.message}</p>}
-            <input type="password" id="password" placeholder="Senha" {...register("password")}/>
+            <input type="password" id="password" placeholder="Senha" {...register("password")} className="password-input"/>
             {errors.password && <p className="error">{errors.password.message}</p>}
-            <input type="password"  id="confimPassword" placeholder="Confirme a senha" {...register("confirmPassword")}/>
+            <input type="password"  id="confimPassword" placeholder="Confirme a senha" {...register("confirmPassword")} className="confirm-input"/>
             {errors.confirmPassword && <p className="error">{errors.confirmPassword.message}</p>}
         </div>
-        <button type="submit" className="submit-button">Criar conta</button>
+        <button type="submit" className="card-button">Criar conta</button>
     </form>
 )
 }
