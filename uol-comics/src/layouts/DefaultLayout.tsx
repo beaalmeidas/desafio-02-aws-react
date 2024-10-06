042de4838af3f80f90427956d295e785e7246e8d
Rafael Soares <rafael.soares.pb@compasso.com.br>
2024-10-05 23:36:00 -0300

    feat: adicionado Header ao layout default

--- uol-comics/src/layouts/DefaultLayout.tsx
+++ uol-comics/src/layouts/DefaultLayout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from "react-router-dom";
-// import Header from "../components/header";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "../components/header";
 
 export function DefaultLayout() {
+  const location = useLocation()
+
+  const filterReturn = (filterValue: string) => {
+    console.log(filterValue)
+  }
+
   return (
     <>
+      {location.pathname !== '/' && (<Header sendFilter={filterReturn}/>)}
       <Outlet/>
     </>
   );
