bce11179d9f81bdaa3e1d7d2d67f45b860991e6b
Debora Oliveira <oliveirasilvadebora@hotmail.com>
2024-10-07 11:51:44 -0300

    docs: adicionando comentários no userForm.tsx

--- uol-comics/src/components/forms/user/UserForm.tsx
+++ uol-comics/src/components/forms/user/UserForm.tsx
@@ -5,7 +5,7 @@ import { SignUpSchema } from "../../../schemas/SignUpSchema"
 import { LoginSchema } from "../../../schemas/LoginSchema"
 import { useNavigate } from "react-router-dom"
 import "./styles.css"
-
+//iniciando a alternancia entre cada form e seu schema
 const UserForm = () => {
     const [isUserLogged, setIsUserLogged] = useState(true)
     const [loadingPage, setLoadingPage] = useState(false)
@@ -13,10 +13,10 @@ const UserForm = () => {
     const handleUserLogin = async (data: LoginSchema) => {
         setLoadingPage(true)
         try{
-            await new Promise((res) => setTimeout(res,4000))
-            localStorage.setItem("UserLogInInfo", JSON.stringify(data))
-            alert("Login finalizado com sucesso!")
-            pageNavegation("/comics-list")
+            await new Promise((res) => setTimeout(res,4000)) // 4s de resposta para ambos
+            localStorage.setItem("UserLogInInfo", JSON.stringify(data)) 
+            alert("Login finalizado com sucesso!") //resposta visual para usuário
+            pageNavegation("/comics-list") 
         }finally{
             setLoadingPage(false)
     }
@@ -33,6 +33,7 @@ const UserForm = () => {
         }
     }
     return (
+        //renderizando condicionalmente cada fom
     <div className="main-container">
         <div className="main-image">
             <img src="../../../../public/assets/images/spider-man.png" alt="spider-man" className="spider-man-image" />
@@ -41,11 +42,12 @@ const UserForm = () => {
         <div className="page-logo">
             <img src="../../../../public/assets/images/uol-comics-logo.png" alt="Uol comics logo" className="uol-comics-logo" />
         </div>
-        <div className={`card ${isUserLogged ? 'login-card' : 'signup-card'}`}>
+        <div className={`card ${isUserLogged ? 'login-card' : 'signup-card'}`}> 
         <h1 className="card-title">{isUserLogged ? "Escolha seu herói" : "Crie seu herói"}</h1>
         {isUserLogged ? (<FormLogin onSubmit={handleUserLogin}/>) : 
             (<FormSignUp onSubmit={handleUserSignIn} />)}
-            {loadingPage && <p className="loading-text">{isUserLogged ? "Aguarde. Logando na sua conta...": "Aguarde. Criando conta..."}</p>}
+            {loadingPage && <p className="loading-text">{isUserLogged ? "Aguarde. Logando na sua conta...": "Aguarde. Criando conta..."}</p>} 
+            {/* resposta visual para o usuário que o login/cadastro funciona */}
         <p className="card-text">{isUserLogged ? "Ainda não tem uma conta?" : "Já tem uma conta?"}
             <a className=" card-text card-link" href="#" onClick={() => setIsUserLogged(!isUserLogged)}>
                 {isUserLogged? " Clique aqui!": " Clique aqui!"}
